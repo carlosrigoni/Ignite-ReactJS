b4f37eff600e5a78d4d258c58fc77e57b59298f0
Carlos Daniel Rigoni <danrigonisocial@gmail.com>
2021-05-29 18:09:58 -0300

    Utilizando o useEffect

--- 01-ReactJS-basics/github-explorer/src/components/RepositoryList.jsx
+++ 01-ReactJS-basics/github-explorer/src/components/RepositoryList.jsx
@@ -1,14 +1,26 @@
+import { useState, useEffect } from "react";
+
 import { RepositoryItem } from "./RepositoryItem"
 
 import '../styles/repositories.scss';
 
+// https://api.github.com/users/icarlosrigoni/repos
+
 const repository = {
   name: "unform",
-  description: "forms in react",
+  description: "Forms in react",
   link: "https://github.com/unform/unform"
 }
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/icarlosrigoni/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+  }, [])
+
   return (
     <section className="repository-list">
       <h1>
